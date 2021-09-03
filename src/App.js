481dafa4f4916b4372df3f57e7df9c88fc336734
Fahad Nadeem <mfahad017@gmail.com>
2021-09-03 15:05:21 +0500

    react helmet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './SCSS/main.scss';
+import { Helmet } from 'react-helmet';
 import HomeHeader from './Components/HomeHeader/HomeHeader';
 import NavigationBar from './Components/NavigationBar/NavigationBar';
 import Contact from './Components/Contact/Contact';
@@ -13,24 +14,59 @@ function App() {
      <Switch>
 
         <Route path="/home">
+          <Helmet>
+            <title>Home Page</title>
+            <meta
+              name="page/home"
+              content="this is the home page of my portfolio"
+            />
+          </Helmet>
         <NavigationBar/>
           <HomeHeader />
         </Route>
         <Route path="/" exact>
+        <Helmet>
+            <title>Home Page</title>
+            <meta
+              name="page/home"
+              content="this is the home page of my portfolio"
+            />
+          </Helmet>
         <NavigationBar/>
           <HomeHeader />
         </Route>
 
         <Route path="/Contact">
+        <Helmet>
+            <title>Contact </title>
+            <meta
+              name="page/contact"
+              content="A page where you can contact me"
+            />
+          </Helmet>
           <NavigationBar/>
           <Contact />
         </Route>
 
         <Route path="/about">
+        <Helmet>
+            <title>about page</title>
+            <meta
+              name="page/about"
+              content="this is a page about me"
+            />
+          </Helmet>
           <NavigationBar/>
           <About />
         </Route>
         <Route path="/projects">
+        <Helmet>
+            <title>Project Page</title>
+            <meta
+              name="page/project"
+              content="this is a page about my projects"
+            />
+          </Helmet>
           <NavigationBar/>
           <ProjectNav />
         </Route>
